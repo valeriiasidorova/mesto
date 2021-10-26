fc3a594d94c712a511c8eca09b79c749457fc1d4
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2021-10-27 01:11:13 +1200

    add pop-up form

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close-button');
+const editButton = document.querySelector('.button_type_edit');
+const popupForm = document.querySelector('.popup__form');
+const inputName = document.querySelector('.popup__input_type_name');
+const profileName = document.querySelector('.profile__name');
+const inputBio = document.querySelector('.popup__input_type_bio');
+const profileBio = document.querySelector('.profile__bio');
+
+function openPopup() {
+  popup.classList.add('popup_is-open');
+  inputName.value = profileName.textContent;
+  inputBio.value = profileBio.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_is-open');
+}
+
+function popupClickHandler(event) {
+  if (event.target.classList.contains('popup')) {
+    closePopup();
+  }
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileBio.textContent = inputBio.value;
+  closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popup.addEventListener('click', popupClickHandler);
+popupForm.addEventListener('submit', submitForm);
