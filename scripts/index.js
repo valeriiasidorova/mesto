e4213c9dfa9f433822cca5ebf8f7eeb29b8de3b5
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2022-01-03 23:28:11 +0300

    feat: add card template

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const editButton = document.querySelector('.button_type_edit');
@@ -7,11 +34,20 @@ const profileName = document.querySelector('.profile__name');
 const inputBio = document.querySelector('.popup__input_type_bio');
 const profileBio = document.querySelector('.profile__bio');
 
-// задача 1: создать темплейт
-// найти в DOM  (".template").content
-// cloneNode('true')
-// forEach
-// append
+// задача 1: создать темплейт для карточек
+const template = document.querySelector('.template').content;
+const cards = document.querySelector('.cards');
+
+initialCards.forEach((el) => {
+  const cardContent = template.cloneNode(true);
+
+  cardContent.querySelector('.card__image').src = el.link;
+  cardContent.querySelector('.card__image').alt = el.name;
+  cardContent.querySelector('.card__title').textContent = el.name;
+
+  cards.append(cardContent);
+});
+
 
 function openPopup() {
   popup.classList.add('popup_is-open');
