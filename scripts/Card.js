664d14105cd083ef06c14a1378dde7e453157f10
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2022-02-16 13:23:31 +0300

    refactor: add Card constructor

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,5 +1,10 @@
 class Card {
-
+  constructor(cardName, cardLink, cardSelector, openPopupFunc) {
+    this._cardName = cardName;
+    this._cardLink = cardLink;
+    this._cardSelector = cardSelector;
+    this._openPopupFunc = openPopupFunc;
+  }
 }
 
 export default Card;
