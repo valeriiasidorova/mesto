d5f96efb5a701d8d288e66bf10c863bce07cd6c8
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2022-04-26 23:40:00 +0300

    refactor file structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import initialCards from './initialCards.js';
-import formConfig from './formConfig.js';
-import Section from './Section.js';
-import Popup from './Popup.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
-import Card from './Card.js';
-import FormValidator from './FormValidator.js';
+import initialCards from '../utils/initialCards.js';
+import formConfig from '../utils/formConfig.js';
+import Section from '../components/Section.js';
+import Popup from '../components/Popup.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
 
 // коллекция попапов для работы с оверлеем
 const popups = Array.from(document.querySelectorAll('.popup'));
