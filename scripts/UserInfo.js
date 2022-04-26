7c1d3f730ac84294b17bd47efb96f1868673b46d
Valeriia Sidorova <valeriia.sidorova@gmail.com>
2022-04-26 11:00:30 +0300

    feat: update UserInfo class

--- scripts/UserInfo.js
+++ scripts/UserInfo.js
@@ -1,13 +1,21 @@
 class UserInfo {
-  constructor(userName, userBio) {
-
+  constructor({ userNameSelector, userBioSelector }) {
+    this._userName = document.querySelector(userNameSelector);
+    this._userBio = document.querySelector(userBioSelector);
   }
 
   getUserInfo() {
+    const userInfo = {};
+    userInfo.name = this._userName.textContent;
+    userInfo.bio = this._userBio.textContent;
 
+    return userInfo;
   }
 
-  setUserInfo() {
-
+  setUserInfo({ name, bio }) {
+    this._userName.textContent = name;
+    this._userBio.textContent = bio;
   }
 }
+
+export default UserInfo;
